627d4500b923acd024d6ac79db91bd37bcb6ed6a
kurapsat000 <77582632+sathvikkurap@users.noreply.github.com>
2025-08-23 18:29:15 -0700

    Simplify GitHub Actions: Remove ninja dependency, create barebones workflow

--- vcpkg/scripts/buildsystems/vcpkg.cmake
+++
@@ -1,3 +0,0 @@
-# Minimal vcpkg.cmake file that does nothing
-# This is just to satisfy the DuckDB build system that expects this file
-message(STATUS "Using minimal vcpkg.cmake - no vcpkg functionality") 
