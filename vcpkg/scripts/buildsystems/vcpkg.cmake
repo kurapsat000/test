003c91ac761c707a804537a8413cd1ac893c8260
kurapsat000 <77582632+sathvikkurap@users.noreply.github.com>
2025-08-23 18:24:35 -0700

    Fix vcpkg issues: Remove vcpkg dependency, use system packages, add C++17 support

---
+++ vcpkg/scripts/buildsystems/vcpkg.cmake
@@ -0,0 +1,3 @@
+# Minimal vcpkg.cmake file that does nothing
+# This is just to satisfy the DuckDB build system that expects this file
+message(STATUS "Using minimal vcpkg.cmake - no vcpkg functionality") 
